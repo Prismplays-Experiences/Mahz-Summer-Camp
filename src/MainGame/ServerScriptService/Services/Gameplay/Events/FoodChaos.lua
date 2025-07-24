7c030a662042f3c2c0aee99e9faf422feec8859d
Mahz <francisatiase@gmail.com>
2025-07-24 17:33:22 +0100

    latest objects folder

--- src/MainGame/ServerScriptService/Services/Gameplay/Events/FoodChaos.lua
+++ src/MainGame/ServerScriptService/Services/Gameplay/Events/FoodChaos.lua
@@ -1,71 +1,68 @@
--- --> Services
--- -----------------------------------------
--- local ReplicatedStorage = game:GetService("ReplicatedStorage")
--- local Debris = game:GetService("Debris")
--- local ServerStorage = game:GetService("ServerStorage")
+--> Services
+-----------------------------------------
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Debris = game:GetService("Debris")
+local ServerStorage = game:GetService("ServerStorage")
 
--- --> Modules
--- -----------------------------------------
--- local Signal = require("@Packages/Signal")
--- local Knit = require("@Packages/Knit")
--- local Trove = require("@Packages/Trove")
+--> Modules
+-----------------------------------------
+local Signal = require("@Packages/Signal")
+local Knit = require("@Packages/Knit")
+local Trove = require("@Packages/Trove")
 
--- local GeneralInfo = require("@Info/GeneralInfo")
+local GeneralInfo = require("@Info/GeneralInfo")
 
--- --> Assets
--- -----------------------------------------
--- local Assets = ServerStorage:WaitForChild("Assets")
--- local Models = ReplicatedStorage:WaitForChild('Models')
--- local ModelAssets = require("@Assets")
--- local ImageAssets = ModelAssets.images
--- local SoundEffects = ReplicatedStorage:WaitForChild("Models"):WaitForChild("SoundEffects")
--- local SpeedBoostModel = Assets:WaitForChild("SpeedBoost")
+--> Assets
+-----------------------------------------
+local Assets = ServerStorage:WaitForChild("Assets")
+local Models = ReplicatedStorage:WaitForChild("Models")
+local ModelAssets = require("@Assets")
+local ImageAssets = ModelAssets.images
+local SoundEffects = ReplicatedStorage:WaitForChild("Models"):WaitForChild("SoundEffects")
+local SpeedBoostModel = Assets:WaitForChild("SpeedBoost")
 
--- local KitchenFoods = ReplicatedStorage:WaitForChild('Models')
+local KitchenFoods = ReplicatedStorage:WaitForChild("Models")
 
--- local ScriptingProperties = workspace:WaitForChild("Game"):WaitForChild("ScriptingProperties")
--- local EventScriptingItems = ScriptingProperties:WaitForChild("Events")
--- local FoodChaosItems = EventScriptingItems:WaitForChild("FoodChaos")
+local ScriptingProperties = workspace:WaitForChild("Game"):WaitForChild("ScriptingProperties")
+local EventScriptingItems = ScriptingProperties:WaitForChild("Events")
+local FoodChaosItems = EventScriptingItems:WaitForChild("FoodChaos")
 
--- --> Setup
--- -----------------------------------------
--- local FoodChaos = Knit.CreateService({
--- 	Name = "FoodChaos",
--- 	Client = {
--- 		EventSubStatus = Knit.CreateProperty(""),
--- 		EventStatus = Knit.CreateProperty(""),
--- 		WeightGained = Knit.CreateSignal(),
--- 	},
--- })
+--> Setup
+-----------------------------------------
+local FoodChaos = Knit.CreateService({
+	Name = "FoodChaos",
+	Client = {
+		EventSubStatus = Knit.CreateProperty(""),
+		EventStatus = Knit.CreateProperty(""),
+		WeightGained = Knit.CreateSignal(),
+	},
+})
 
--- --> Variables
--- -----------------------------------------
--- FoodChaos.ModeEnded = Signal.new()
--- FoodChaos.ExplodeTime = 60
--- FoodChaos.Trove = Trove.new()
--- FoodChaos.MaxWeightGained = 250
--- FoodChaos.MinPlayers = 2 -- 2
--- FoodChaos.LockWorkoutMachines = true
--- FoodChaos.YieldClock = true
--- FoodChaos.Ended = Signal.new()
--- FoodChaos.Details = {
--- 	Text = "Food chaos!",
--- 	Image = ImageAssets.FoodChaos,
--- }
+--> Variables
+-----------------------------------------
+FoodChaos.ModeEnded = Signal.new()
+FoodChaos.ExplodeTime = 60
+FoodChaos.Trove = Trove.new()
+FoodChaos.MaxWeightGained = 250
+FoodChaos.MinPlayers = 2 -- 2
+FoodChaos.LockWorkoutMachines = true
+FoodChaos.YieldClock = true
+FoodChaos.Ended = Signal.new()
+FoodChaos.Details = {
+	Text = "Food chaos!",
+	Image = ImageAssets.FoodChaos,
+}
 
--- --> Utility Functions
--- -----------------------------------------
+--> Utility Functions
+-----------------------------------------
 
--- function RandomFood()
+function RandomFood()
+	local Food = KitchenFoods:GetChildren()[math.random(1, #KitchenFoods:GetChildren())]
 
---     return KitchenFoods:GetChildren()[math.random(1,# KitchenFoods:GetChildren())]
---     workspace.
+	return
+end
 
--- end
+function DropFood(Count) end
 
--- function DropFood(Count)
-
--- end
-
--- --> Main Functions
--- -----------------------------------------
+--> Main Functions
+-----------------------------------------
